6a4382e1277d209329344152cfcd36624ebfa435
KirillIv2003 <kirill2003iv@gmail.com>
2023-09-20 17:36:53 +0300

    Добавьте файлы проекта.

---
+++ cnsHello/cnsHello/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Имя?");
+string name = Console.ReadLine();
+Console.WriteLine("Город?");
+string city = Console.ReadLine();
+
+Console.WriteLine("Имя = " + name + ", Город =" + city);
+Console.WriteLine("Имя = {0}, Город = {1}", name, city);
+Console.WriteLine($"Имя = {name}, Город = {city}");
---
+++ sncDrawLine/sncDrawLine/Program.cs
@@ -0,0 +1,21 @@
+﻿bool isContinue;
+
+do
+{
+    Console.WriteLine("Длинна?");
+
+    int.TryParse(Console.ReadLine(), out int width);
+
+    //1
+    //for (int i = 0; i < width; i++)
+    //{
+    //    Console.WriteLine("*");
+    //}
+    //Console.WriteLine();
+
+    //(2)
+    Console.WriteLine(new String('*', width));
+
+    Console.Write("Повторить? [Y/N] ->");
+    isContinue = Console.ReadLine()?.ToUpper() == "Y";
+} while (isContinue);

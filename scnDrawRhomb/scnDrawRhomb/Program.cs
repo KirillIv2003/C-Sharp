1c5ce4de19c5008389c8dd9387d28ccd161184d2
KirillIv2003 <kirill2003iv@gmail.com>
2023-09-20 20:54:24 +0300

    Ромб

---
+++ scnDrawRhomb/scnDrawRhomb/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 5; // Размер ромба (можете изменить на желаемое значение)
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("/");
+
+            for (int j = 2; j < 2 * i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("\\");
+
+            Console.WriteLine();
+        }
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("\\");
+
+            for (int j = 2; j < 2 * i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("/");
+
+            Console.WriteLine();
+        }
+    }
+}
+
